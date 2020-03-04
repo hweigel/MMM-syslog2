15c3554a85eff35c16db9c56862aef0a559dd8de
hweigel <60696861+hweigel@users.noreply.github.com>
2020-03-04 02:19:53 +0100

    Update MMM-syslog2.js

--- MMM-syslog2.js
+++ MMM-syslog2.js
@@ -97,8 +97,8 @@ Module.register('MMM-syslog2',{
 				message = message.slice(0, this.config.shortenMessage) + "&#8230;";
 			}
 			//Set caller of row
-			var caller =  document.createElement("i");
-			caller.innerHTML = "&nbsp&nbsp" + message;
+			var caller =  document.createElement("&nbsp");
+			caller.innerHTML = "&nbsp" + message;
 			caller.classList.add("title", "small", "align-left");
 			if(this.config.types.hasOwnProperty(this.messages[i].type)){
 				caller.classList.add(this.config.types[this.messages[i].type]);
